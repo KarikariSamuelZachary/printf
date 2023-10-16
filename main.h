92dfdf32530e49132e888f07aa14a8bbdf1e8efc
KarikariSamuelZachary <skarikaripresecghana@gmail.com>
2023-10-16 18:17:49 +0100

    New changes

--- main.h
+++ main.h
@@ -4,5 +4,11 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
+int _putchar(char c);
+int printf_char(va_list val);
+int printf_string(va_list val);	
+int strlen(char *str);
+int strlenc(const char *str);
+int print_37(void);
 
 #endif /* MAIN_H */
---
+++ print_strlen.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @str: string pointer
+ * Return: string pointer
+ */
+
+int strlen(char *str)
+{
+	int i;
+	for (i = 0; str[i] != 0; i++)
+		;
+	return (i);
+}
+
+/**
+ * _strlenc - Srlen function but applied for constant char pointer str
+ * @str: char pointer
+ * Return: i
+ */
+
+int _strlenc(const char *str)
+{
+	int i;
+
+	for (i = 0; str[i] !=0; i++)
+		;
+	return (i);
+}
